61164ee3eb08c9c932ffcb4d7d2488042bd5a2b0
renewensel <hey@renewensel.com>
2023-12-08 14:30:26 +0100

    add marker.svg to map

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import MapWithNoSSR from "@/components/MapWithNoSSR";
 import About from "@/components/About";
+import Navigation from "@/components/Navigation";
 
 const MapPage = () => {
     return (
         <>
+            <Navigation className="header" />
             <h1>Map Page</h1>
             <div>
-                <MapWithNoSSR />
+                <MapWithNoSSR style={{ zIndex: 12 }} />
             </div>
         </>
     );
