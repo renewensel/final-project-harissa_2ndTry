9bef2ab47ab988247f8e37f9249ef62900bd1d9a
renewensel <hey@renewensel.com>
2023-12-11 21:57:30 +0100

    remove Array.isArray -> item.ingredients

--- components/AdminDashboard/index.js
+++ components/AdminDashboard/index.js
@@ -20,146 +20,140 @@ const AdminDashboard = () => {
         return <div>Loading...</div>;
     }
 
-    const renderTable = (data, filter, setFilter) => (
-        <div className={styles.tableContainer}>
-            <h2>Dishes</h2>
-            <div className={styles.buttonsContainer}>
-                <button
-                    className={
-                        filter === "All"
-                            ? styles.activeButtonNewStyleAllActive
-                            : styles.activeButtonNewStyleAll
-                    }
-                    onClick={() => setFilter("All")}
-                >
-                    ALL
-                </button>
-                <button
-                    className={
-                        filter === "true"
-                            ? styles.activeButtonNewStyleOnlineActive
-                            : styles.activeButtonNewStyleOnline
-                    }
-                    onClick={() => setFilter("true")}
-                >
-                    Online
-                </button>
-                <button
-                    className={
-                        filter === "false"
-                            ? styles.activeButtonNewStyleOfflineActive
-                            : styles.activeButtonNewStyleOffline
-                    }
-                    onClick={() => setFilter("false")}
-                >
-                    Offline
-                </button>
-            </div>
-            <div>
-                {data
-                    .filter((item) => {
-                        if (filter === "All") return true;
-                        return String(item.isShown) === filter;
-                    })
-                    .map((item) => (
-                        <li
-                            key={item._id}
-                            className={`list dish-li ${
-                                String(item.isShown) === "false"
-                                    ? styles.hiddenDish
-                                    : ""
-                            }`}
-                        >
-                            <div className="default-dish-image">
-                                <Image
-                                    src={item.dishImage}
-                                    alt={item.dish}
-                                    width={100}
-                                    height={100}
-                                />
-                            </div>
-                            <div>
-                                {Object.entries(item.flavour)
-                                    .filter(([key, value]) => value === true)
-                                    .map(([key]) => (
-                                        <p
-                                            key={key}
-                                            className="badge"
-                                            style={{ fontSize: "0.7rem" }}
-                                        >
-                                            {key}
-                                        </p>
-                                    ))}
-                            </div>
-                            <div className="dish-list-name-ingredients-spaces">
-                                <div className="dishes-dish-name">
-                                    {item.dish}&nbsp;
-                                    <span>
-                                        <DishIcon
-                                            type="meat"
-                                            isTrue={item.ingredientsIcons.meat}
-                                        />
-                                        <DishIcon
-                                            type="vegetarian"
-                                            isTrue={
-                                                item.ingredientsIcons.vegetarian
-                                            }
-                                        />
-                                        <DishIcon
-                                            type="vegan"
-                                            isTrue={item.ingredientsIcons.vegan}
-                                        />
-                                    </span>
-                                </div>
-                                <div>
-                                    <p className="dish-ingredients">
-                                        {Array.isArray(item.ingredients)
-                                            ? item.ingredients.map(
-                                                  (ingredient, index) => (
-                                                      <p key={index}>
-                                                          {ingredient}
-                                                      </p>
-                                                  )
-                                              )
-                                            : item.ingredients}
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="dish-price">
-                                <p>5,90€</p>
-                            </div>
-                            <div>
-                                <button
-                                    style={{ pointerEvents: "none" }}
-                                    className={`status-button-${
-                                        String(item.isShown) === "true"
-                                            ? "online"
-                                            : "offline"
-                                    }`}
-                                >
-                                    {String(item.isShown) === "true"
-                                        ? "Online"
-                                        : "Offline"}
-                                </button>
-                                <button className="status-button-check-on">
-                                    ✔ Add
-                                </button>
-                                <button className="status-button-check-off">
-                                    X Remove
-                                </button>
-                            </div>
-                        </li>
-                    ))}
-            </div>
-        </div>
-    );
-
     return (
         <>
             <div className={styles.dateStatusButtonsBox}>
                 <h5 className={styles.adminH4}>Dishes</h5>
             </div>
-            {renderTable(dishes, dishFilter, setDishFilter)}
+            <div className={styles.tableContainer}>
+                <div className={styles.buttonsContainer}>
+                    <button
+                        className={
+                            dishFilter === "All"
+                                ? styles.activeButtonNewStyleAllActive
+                                : styles.activeButtonNewStyleAll
+                        }
+                        onClick={() => setDishFilter("All")}
+                    >
+                        ALL
+                    </button>
+                    <button
+                        className={
+                            dishFilter === "true"
+                                ? styles.activeButtonNewStyleOnlineActive
+                                : styles.activeButtonNewStyleOnline
+                        }
+                        onClick={() => setDishFilter("true")}
+                    >
+                        Online
+                    </button>
+                    <button
+                        className={
+                            dishFilter === "false"
+                                ? styles.activeButtonNewStyleOfflineActive
+                                : styles.activeButtonNewStyleOffline
+                        }
+                        onClick={() => setDishFilter("false")}
+                    >
+                        Offline
+                    </button>
+                </div>
+                <div>
+                    {dishes
+                        .filter((item) => {
+                            if (dishFilter === "All") return true;
+                            return String(item.isShown) === dishFilter;
+                        })
+                        .map((item) => (
+                            <li
+                                key={item._id}
+                                className={`list dish-li ${
+                                    String(item.isShown) === "false"
+                                        ? styles.hiddenDish
+                                        : ""
+                                }`}
+                            >
+                                <div className="default-dish-image">
+                                    <Image
+                                        src={item.dishImage}
+                                        alt={item.dish}
+                                        width={100}
+                                        height={100}
+                                    />
+                                </div>
+                                <div>
+                                    {Object.entries(item.flavour)
+                                        .filter(
+                                            ([key, value]) => value === true
+                                        )
+                                        .map(([key]) => (
+                                            <p
+                                                key={key}
+                                                className="badge"
+                                                style={{ fontSize: "0.7rem" }}
+                                            >
+                                                {key}
+                                            </p>
+                                        ))}
+                                </div>
+                                <div className="dish-list-name-ingredients-spaces">
+                                    <div className="dishes-dish-name">
+                                        {item.dish}&nbsp;
+                                        <span>
+                                            <DishIcon
+                                                type="meat"
+                                                isTrue={
+                                                    item.ingredientsIcons.meat
+                                                }
+                                            />
+                                            <DishIcon
+                                                type="vegetarian"
+                                                isTrue={
+                                                    item.ingredientsIcons
+                                                        .vegetarian
+                                                }
+                                            />
+                                            <DishIcon
+                                                type="vegan"
+                                                isTrue={
+                                                    item.ingredientsIcons.vegan
+                                                }
+                                            />
+                                        </span>
+                                    </div>
+                                    <div>
+                                        <p className="dish-ingredients">
+                                            {item.ingredients}
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="dish-price">
+                                    <p>5,90€</p>
+                                </div>
+                                <div>
+                                    <button
+                                        style={{ pointerEvents: "none" }}
+                                        className={`status-button-${
+                                            String(item.isShown) === "true"
+                                                ? "online"
+                                                : "offline"
+                                        }`}
+                                    >
+                                        {String(item.isShown) === "true"
+                                            ? "Online"
+                                            : "Offline"}
+                                    </button>
+                                    <button className="status-button-check-on">
+                                        ✔ Add
+                                    </button>
+                                    <button className="status-button-check-off">
+                                        X Remove
+                                    </button>
+                                </div>
+                            </li>
+                        ))}
+                </div>
+            </div>
         </>
     );
 };
