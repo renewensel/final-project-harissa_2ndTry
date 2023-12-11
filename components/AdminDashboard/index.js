02e0b8942c12dfeefb262f1752b24ed111a22178
renewensel <hey@renewensel.com>
2023-12-11 18:10:52 +0100

    more more css for the adminDashboard

--- components/AdminDashboard/index.js
+++ components/AdminDashboard/index.js
@@ -1,6 +1,7 @@
 // pages/index.js
 import useSWR from "swr";
 import { useState } from "react";
+import Image from "next/image";
 
 import styles from "@/styles/adminDashboard.module.css";
 
@@ -23,7 +24,7 @@ const AdminDashboard = () => {
 
     const renderTable = (data, title, filter, setFilter) => (
         <div className={styles.tableContainer}>
-            {/* <h2>{title}</h2> */}
+            <h2>{title}</h2>
             <div className={styles.buttonsContainer}>
                 <button
                     className={
@@ -57,13 +58,6 @@ const AdminDashboard = () => {
                 </button>
             </div>
             <table className={styles.table}>
-                {/* <thead>
-                    <tr>
-                        <th>Dish/Drink</th>
-                        {title === "Dishes" && <th>Ingredients</th>}
-                        <th>Status</th>
-                    </tr>
-                </thead> */}
                 <tbody>
                     {data
                         .filter((item) => {
@@ -72,9 +66,27 @@ const AdminDashboard = () => {
                         })
                         .map((item) => (
                             <tr key={item._id}>
+                                <td>
+                                    <Image
+                                        src={
+                                            title === "Dishes"
+                                                ? item.dishImage
+                                                : item.drinkImage
+                                        }
+                                        alt={
+                                            title === "Dishes"
+                                                ? item.dish
+                                                : item.drink
+                                        }
+                                        width={title === "Dishes" ? 100 : 45}
+                                        height={100}
+                                    />
+                                </td>
                                 <td>{item.dish || item.drink}</td>
                                 {title === "Dishes" && (
-                                    <td>{item.ingredients}</td>
+                                    <td className={styles.ingredients}>
+                                        {item.ingredients}
+                                    </td>
                                 )}
                                 <td>{item.isShown ? "Online" : "Offline"}</td>
                             </tr>
@@ -149,15 +161,6 @@ const AdminDashboard = () => {
                         Mon, Dec 11, 2023 - Fri, Dec 15, 2023
                     </span>
                 </h4>
-                <div className={styles.buttonStatusAdminBox}>
-                    {/* <button className={styles.adminDashboardButton}>All</button>
-                    <button className={styles.adminDashboardButton}>
-                        Online
-                    </button>
-                    <button className={styles.adminDashboardButton}>
-                        Offline
-                    </button> */}
-                </div>
             </div>
             {renderTable(drinks, "Drinks", drinkFilter, setDrinkFilter)}
         </>
